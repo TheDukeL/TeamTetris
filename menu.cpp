0692ee8ffb3a07f3b6d554fe6f88a715e9532e70
AmberH1 <49109391+AmberH1@users.noreply.github.com>
2019-04-29 00:01:10 -0500

    Update menu.cpp

--- menu.cpp
+++ menu.cpp
@@ -5,22 +5,32 @@ tetris_window::tetris_window()
 {
   set_border_width(10);
 
-  image.set("tetris.png");
+  image.set("tetrismenubackground1.png");
   grid.attach(image,0,0,3,1);
 
+  
+  
+  
   start.add_label("Start");
   //
   grid.attach(start,0,1,1,1);
 
+  
 
 
   settings.add_label("Settings");
   //
   grid.attach(settings,1,1,1,1);
 
+  
   help.add_label("Help");
   //
   grid.attach(help,2,1,1,1);
+  ////
+  help.signal_pressed().connect(sigc::mem_fun(*this,&tetris_window::helping));
+  ////
+  
+  
 
   grid.show_all();
 
@@ -29,3 +39,7 @@ tetris_window::tetris_window()
 
 tetris_window::~tetris_window()
 {}
+////
+void tetris_window::helping(){
+  
+}
